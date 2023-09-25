d14aed59655e1a22bed6b94258658d94e83e0790
Lien Zhu <lienzhucs@gmail.com>
2023-09-24 20:54:58 -0700

    adds delay

--- simulate.py
+++ simulate.py
@@ -1,5 +1,12 @@
 import pybullet as p
+import time
 
 physics_client = p.connect(p.GUI)
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/120.0)
+    print(i)
 
 p.disconnect()

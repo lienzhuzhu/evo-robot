b46fd902746afd5c2077cf0241dd3b61824531c8
Lien Zhu <lienzhucs@gmail.com>
2023-09-25 09:48:26 -0700

    uses constants

--- generate.py
+++ generate.py
@@ -1,8 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 
+LENGTH  = 1
+WIDTH   = 1
+HEIGHT  = 1
+
 
 pyrosim.Start_SDF("box.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[LENGTH, WIDTH, HEIGHT])
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -2,12 +2,14 @@ import pybullet as p
 import pybullet_data
 import time
 
+GRAVITY = -9.8
+
 physics_client = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
-p.setGravity(0,0,-9.8)
+p.setGravity(0, 0, GRAVITY)
 plane_id = p.loadURDF("plane.urdf")
 
 

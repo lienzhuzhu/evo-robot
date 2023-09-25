3151072b7c4d2e31b88dbd1da66a0333e3902f71
Lien Zhu <lienzhucs@gmail.com>
2023-09-25 09:57:43 -0700

    modifies one link dimensions and creates it at ground level

--- generate.py
+++ generate.py
@@ -1,5 +1,9 @@
 import pyrosim.pyrosim as pyrosim
 
+X   = 0
+Y   = 0
+Z   = 1.5
+
 LENGTH  = 1
 WIDTH   = 2
 HEIGHT  = 3
@@ -7,6 +11,6 @@ HEIGHT  = 3
 
 pyrosim.Start_SDF("box.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[LENGTH, WIDTH, HEIGHT])
+pyrosim.Send_Cube(name="Box", pos=[X, Y, Z] , size=[LENGTH, WIDTH, HEIGHT])
 
 pyrosim.End()

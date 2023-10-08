a7285af1d3dce18916dbff2724644c3b8431f194
Lien Zhu <lienzhucs@gmail.com>
2023-10-07 23:31:40 -0700

    finishes E.45

--- generate.py
+++ generate.py
@@ -17,9 +17,27 @@ def Create_World():
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
+
     pyrosim.Send_Cube(name="Link0", pos=[X, Y, Z] , size=[LENGTH, WIDTH, HEIGHT])
-    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [X+0.5,Y,Z+0.5] )
-    pyrosim.Send_Cube(name="Link1", pos=[0.5, 0, 0.5] , size=[LENGTH, WIDTH, HEIGHT])
+    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [X, Y, Z+0.5] )
+
+    pyrosim.Send_Cube(name="Link1", pos=[0, 0, 0.5] , size=[LENGTH, WIDTH, HEIGHT])
+    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0, 0, 1.0] )
+
+    pyrosim.Send_Cube(name="Link2", pos=[0, 0, 0.5] , size=[LENGTH, WIDTH, HEIGHT])
+    pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0, 0.5, 0.5] )
+
+    pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0.0] , size=[LENGTH, WIDTH, HEIGHT])
+    pyrosim.Send_Joint( name = "Link3_Link4" , parent= "Link3" , child = "Link4" , type = "revolute", position = [0, 1, 0.0] )
+
+    pyrosim.Send_Cube(name="Link4", pos=[0, 0.5, 0.0] , size=[LENGTH, WIDTH, HEIGHT])
+    pyrosim.Send_Joint( name = "Link4_Link5" , parent= "Link4" , child = "Link5" , type = "revolute", position = [0, 0.5, -0.5] )
+
+    pyrosim.Send_Cube(name="Link5", pos=[0, 0, -0.5] , size=[LENGTH, WIDTH, HEIGHT])
+    pyrosim.Send_Joint( name = "Link5_Link6" , parent= "Link5" , child = "Link6" , type = "revolute", position = [0, 0.0, -1.0] )
+
+    pyrosim.Send_Cube(name="Link6", pos=[0, 0, -0.5] , size=[LENGTH, WIDTH, HEIGHT])
+
     pyrosim.End()
 
 

a78768e73436279d8c8516bcaff046498df44b58
Lien Zhu <lienzhucs@gmail.com>
2023-10-08 16:06:00 -0700

    finished E joints

--- generate.py
+++ generate.py
@@ -18,25 +18,13 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
 
-    pyrosim.Send_Cube(name="Link0", pos=[X, Y, Z] , size=[LENGTH, WIDTH, HEIGHT])
-    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [X, Y, Z+0.5] )
+    pyrosim.Send_Cube(name="Torso", pos=[X+1.5, Y, Z+1.0] , size=[LENGTH, WIDTH, HEIGHT])
 
-    pyrosim.Send_Cube(name="Link1", pos=[0, 0, 0.5] , size=[LENGTH, WIDTH, HEIGHT])
-    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0, 0, 1.0] )
+    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [X+1.0, Y, Z+0.5])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5] , size=[LENGTH, WIDTH, HEIGHT])
 
-    pyrosim.Send_Cube(name="Link2", pos=[0, 0, 0.5] , size=[LENGTH, WIDTH, HEIGHT])
-    pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0, 0.5, 0.5] )
-
-    pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0.0] , size=[LENGTH, WIDTH, HEIGHT])
-    pyrosim.Send_Joint( name = "Link3_Link4" , parent= "Link3" , child = "Link4" , type = "revolute", position = [0, 1, 0.0] )
-
-    pyrosim.Send_Cube(name="Link4", pos=[0, 0.5, 0.0] , size=[LENGTH, WIDTH, HEIGHT])
-    pyrosim.Send_Joint( name = "Link4_Link5" , parent= "Link4" , child = "Link5" , type = "revolute", position = [0, 0.5, -0.5] )
-
-    pyrosim.Send_Cube(name="Link5", pos=[0, 0, -0.5] , size=[LENGTH, WIDTH, HEIGHT])
-    pyrosim.Send_Joint( name = "Link5_Link6" , parent= "Link5" , child = "Link6" , type = "revolute", position = [0, 0.0, -1.0] )
-
-    pyrosim.Send_Cube(name="Link6", pos=[0, 0, -0.5] , size=[LENGTH, WIDTH, HEIGHT])
+    pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [X+2.0, Y, Z+0.5])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5] , size=[LENGTH, WIDTH, HEIGHT])
 
     pyrosim.End()
 

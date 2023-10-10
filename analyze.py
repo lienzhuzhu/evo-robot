7cd926460da1ad8ad4fe2528fa2de785aa21236a
Lien Zhu <lienzhucs@gmail.com>
2023-10-09 17:52:24 -0700

    finishes step 36

--- analyze.py
+++ analyze.py
@@ -2,4 +2,5 @@ import numpy
 import matplotlib.pyplot
 
 backLeg_sensor_data = numpy.load("data/sensor.npy")
-print(backLeg_sensor_data)
+matplotlib.pyplot.plot(backLeg_sensor_data)
+matplotlib.pyplot.show()

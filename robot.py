17ebd1015450486fd48227c0567f7f714a345b1a
Lien Zhu <lienzhucs@gmail.com>
2023-12-08 16:21:29 -0800

    H.48

--- robot.py
+++ robot.py
@@ -8,8 +8,14 @@ from sensor import SENSOR
 
 class ROBOT:
     def __init__(self):
-        self.sensors = {}
         self.motors = {}
 
         self.robot_id = p.loadURDF("./body.urdf")
         pyrosim.Prepare_To_Simulate(self.robot_id)
+
+        self.Prepare_To_Sense()
+
+    def Prepare_To_Sense(self):
+        self.sensors = {}
+        for linkName in pyrosim.linkNamesToIndices:
+            print(linkName)

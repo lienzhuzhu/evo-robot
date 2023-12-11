369dc19750007a3dabd04aaa2d1a6639e8adb68e
Lien Zhu <lienzhucs@gmail.com>
2023-12-10 16:41:53 -0800

    I.85

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -50,7 +50,7 @@ class NEURON:
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
 
     def Update_Hidden_Or_Motor_Neuron(self):
-        self.Set_Value(math.pi / 4.)
+        self.Set_Value(0.0)
 
 
     def Is_Hidden_Neuron(self):

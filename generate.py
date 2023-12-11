430876347792a09120661412dcf482feae545552
Lien Zhu <lienzhucs@gmail.com>
2023-12-10 21:13:47 -0800

    K.14 flips on its head

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 
 X   = 0
@@ -45,16 +46,9 @@ def Generate_Brain():
     pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
 
-
-    pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = 0.0 )
-    pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = -2.0 )
-
-    pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = 0.0 )
-    pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = 0.0 )
-
     for i in range(3):
-        for j in range(2):
-            pass
+        for j in range(3,5):
+            pyrosim.Send_Synapse( sourceNeuronName = i , targetNeuronName = j , weight = random.random() * 2 - 1 )
 
     pyrosim.End()
 

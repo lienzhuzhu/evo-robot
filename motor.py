96189845c923dd1c6fb5ef565ca99bbd8949ab4e
Lien Zhu <lienzhucs@gmail.com>
2023-12-10 16:43:54 -0800

    I.86

--- motor.py
+++ motor.py
@@ -12,17 +12,17 @@ class MOTOR:
         self.Prepare_To_Act()
 
 
-    def Prepare_To_Act(self):
-        self.amplitude  = c.AMPLITUDE
-        self.frequency  = c.FREQUENCY
-        self.offset     = c.PHASE_OFFSET
+    #def Prepare_To_Act(self):
+    #    self.amplitude  = c.AMPLITUDE
+    #    self.frequency  = c.FREQUENCY
+    #    self.offset     = c.PHASE_OFFSET
 
-        if self.jointName == "Torso_BackLeg":
-            self.amplitude = np.pi / 2
-            self.offset = 0
+    #    if self.jointName == "Torso_BackLeg":
+    #        self.amplitude = np.pi / 2
+    #        self.offset = 0
 
-        x = np.linspace(0, 2*np.pi*c.TIMESTEPS/1000, c.TIMESTEPS)
-        self.motor_values   = self.amplitude * np.sin(self.frequency * x + self.offset)
+    #    x = np.linspace(0, 2*np.pi*c.TIMESTEPS/1000, c.TIMESTEPS)
+    #    self.motor_values   = self.amplitude * np.sin(self.frequency * x + self.offset)
 
     def Set_Value(self, desiredAngle, robot_id):
         pyrosim.Set_Motor_For_Joint(
@@ -33,8 +33,8 @@ class MOTOR:
             maxForce = c.MAX_FORCE
         )
 
-    def Save_Values(self):
-        np.save("data/" + self.jointName + ".npy", self.motor_values)
+    #def Save_Values(self):
+    #    np.save("data/" + self.jointName + ".npy", self.motor_values)
 
 
 """

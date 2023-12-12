f73df788cf8e79416f1707b3da2f4370cf67172d
Lien Zhu <lienzhucs@gmail.com>
2023-12-11 22:36:52 -0800

    L.85

--- solution.py
+++ solution.py
@@ -17,7 +17,7 @@ class SOLUTION:
         self.Create_Body()
         self.Create_Brain()
 
-        os.system("python3 simulate.py")
+        os.system("python3 simulate.py DIRECT")
 
         with open("fitness.txt", "r") as file:
             self.fitness = float(file.read())

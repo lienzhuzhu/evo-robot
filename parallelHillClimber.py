037e3dcdbabd3a6c32778d460425e81e5e22a718
Lien Zhu <lienzhucs@gmail.com>
2023-12-13 23:42:15 -0800

    M.90

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -25,28 +25,30 @@ class PARALLEL_HILL_CLIMBER:
 
 
     def Evolve(self):
-        #for parent in self.parents.values():
-        #    parent.Start_Simulation("DIRECT")
+        for parent in self.parents.values():
+            parent.Start_Simulation("DIRECT")
 
-        #for parent in self.parents.values():
-        #    parent.Wait_For_Simulation_To_End()
+        for parent in self.parents.values():
+            parent.Wait_For_Simulation_To_End()
 
-        #for currentGeneration in range(c.numberOfGenerations):
-        #    self.Evolve_For_One_Generation()
-        pass
+        for currentGeneration in range(c.numberOfGenerations):
+            self.Evolve_For_One_Generation()
 
 
     def Evolve_For_One_Generation(self):
         self.Spawn()
-        self.Mutate()
-        self.child.Evaluate("DIRECT")
-        self.Print()
-        self.Select()
+        #self.Mutate()
+        #self.child.Evaluate("DIRECT")
+        #self.Print()
+        #self.Select()
 
     def Spawn(self):
-        self.child = copy.deepcopy(self.parent)
-        self.child.Set_ID(self.nextAvailableID)
-        self.nextAvailableID += 1
+        self.children = {}
+        for i, parent in enumerate(self.parents.values()):
+            self.children[i] = copy.deepcopy(parent)
+            self.children[i].Set_ID(self.nextAvailableID)
+            self.nextAvailableID += 1
+
 
     def Mutate(self):
         self.child.Mutate()
